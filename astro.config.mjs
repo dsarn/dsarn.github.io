466440c70020774f9d4a8eab08c0467cd991d7a6
Benk <dsarniske@gmail.com>
2026-04-23 17:06:30 -0700

    deploy.yml updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://dsarn.github.io',
-    base: '/dsarn.github.io'
+    site: 'https://dsarn.github.io'
 });
